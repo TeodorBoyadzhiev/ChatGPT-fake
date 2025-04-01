f8dbab525645dd54f9453259d2f64523b2cfe3b8
Teodor <teodor1497@gmail.com>
2025-04-01 16:03:02 +0300

    Routes protection

--- src/routes/signInPage/SignInPage.jsx
+++ src/routes/signInPage/SignInPage.jsx
@@ -1,9 +1,10 @@
 import { SignIn } from '@clerk/clerk-react';
+import "./signInPageStyle.css";
 
 const SignInPage = () => {
   return (
     <div className='signInPage'>
-        <SignIn path="/sign-in" />
+        <SignIn path="/sign-in" signUpUrl="/sign-up" />
     </div>
   );
 };
