ca18b5b6cdb0f64cced5c866c1beab98fa3eeb14
Teodor <teodor1497@gmail.com>
2025-04-16 19:47:20 +0300

    Homepage elements and styles

--- src/routes/landingPage/LandingPage.jsx
+++ src/routes/landingPage/LandingPage.jsx
@@ -4,7 +4,23 @@ import './landingpage.css'
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>ChatGpt</h1>
+        <h2>Supercharge your creatifity</h2>
+        <h3>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+          If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+        </div>
+      </div>
     </div>
   )
 }
