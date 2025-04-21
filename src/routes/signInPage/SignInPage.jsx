4e9eba9850173cd242c1c0ed93f041294b85cc37
Teodor <teodor1497@gmail.com>
2025-04-21 21:32:21 +0300

    redirect to dashboard after login

--- src/routes/signInPage/SignInPage.jsx
+++ src/routes/signInPage/SignInPage.jsx
@@ -4,7 +4,7 @@ import "./signInPageStyle.css";
 const SignInPage = () => {
   return (
     <div className='signInPage'>
-        <SignIn path="/sign-in" signUpUrl="/sign-up" />
+        <SignIn path="/sign-in" signUpUrl="/sign-up" forceRedirectUrl="/dashboard" />
     </div>
   );
 };
