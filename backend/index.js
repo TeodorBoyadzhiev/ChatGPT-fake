0a2ef9eba95d750a883a1b5f20ae4eec178c3e16
Teodor <teodor1497@gmail.com>
2025-04-24 18:40:53 +0300

    initialize imagekit

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,10 @@ const app = express();
 // SDK initialization
 
 
-// var imagekit = new ImageKit({
-//     urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
-//     publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
-//     privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
-// });
-
 var imagekit = new ImageKit({
-    urlEndpoint: "https://ik.imagekit.io/6rb8jp8qk",
-    publicKey: "public_XSUsNJsUTLJBoLpFdpAqq1GPOvA=",
-    privateKey: "private_0AR7oV/9l6gyhYQMX5tg4b9jujs="
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
 });
 
 app.get("/api/upload", (req, res) => {
