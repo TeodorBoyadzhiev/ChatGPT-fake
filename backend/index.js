9b5d0851ee0bbf7bb884c37e97fb40810799d3e7
Teodor <teodor1497@gmail.com>
2025-05-04 20:04:07 +0300

    add MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import ImageKit from "imagekit";
 import cors from "cors";
+import mongoose from "mongoose";
+import * as data from "./util.js";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -8,6 +10,24 @@ const app = express();
 app.use(cors({
   origin: "http://localhost:5173",
 }));
+
+const connect = () => {
+    return new Promise((resolve, reject) => {
+
+        mongoose.connect(data.default.DB_CONNECTION_STRING, {});
+
+        const db = mongoose.connection;
+
+        db.on('error', (err) => {
+            console.error('connection error:', err);
+            reject(err);
+        });
+        db.once('open', function () {
+            console.log('Database ready');
+            resolve();
+        });
+    });
+};
 
 // SDK initialization
 
@@ -23,6 +43,6 @@ app.get("/api/upload", (req, res) => {
 });
 
 app.listen(port, () => {
-//   connect();
+  connect();
   console.log("Server running on 3000");
 });
