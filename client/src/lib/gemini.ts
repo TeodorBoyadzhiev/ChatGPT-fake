7cc0685fc876607b1b62490dab2bb621a411041c
Teodor <teodor1497@gmail.com>
2025-05-16 17:38:07 +0300

    make gemmini ai remember context of out chat

--- client/src/lib/gemini.ts
+++ client/src/lib/gemini.ts
@@ -15,22 +15,31 @@ export const ai = new GoogleGenAI({
   apiKey: import.meta.env.VITE_GEMINI_PUBLIC_KEY,
 });
 
+// Тук пазим историята на разговора
+const chatHistory: { role: "user" | "model"; parts: { text: string }[] }[] = [
+  {
+    role: "user",
+    parts: [{ text: "Hello" }],
+  },
+  {
+    role: "model",
+    parts: [{ text: "Great to meet you. What would you like to know?" }],
+  },
+];
+
+
 export async function main(text: string): Promise<string> {
+  chatHistory.push({
+    role: "user",
+    parts: [{ text }],
+  });
+
   const chat = ai.chats.create({
     model: "gemini-2.0-flash",
     config: {
       safetySettings: safetySettings,
     },
-    history: [
-      {
-        role: "user",
-        parts: [{ text: "Hello" }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-      },
-    ],
+    history: chatHistory
   });
 
   const stream = await chat.sendMessageStream({
